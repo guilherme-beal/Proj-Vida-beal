d7709b8c02f4cbd4bd993cbbd2b8af657d3edbab
Guilherme Beal <108025071+guilherme-beal@users.noreply.github.com>
2024-04-11 17:29:47 -0300

    11/04

--- main.js
+++ main.js
@@ -5,8 +5,29 @@ for (let i = 0; i < botoes.length; i++){
 
         for(let j=0; j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            botoes[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-07-08T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+
+
+
+
+
+
+}
+
+
+
+
